bbdc71b663ee8219955649561d625a9d9143998d
claauzuma <c.zumarraga94@hotmail.com>
2024-11-21 23:38:22 -0300

    M

--- proyecto01/vite.config.js
+++ proyecto01/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Resuelve problemas con importaciones
+      axios: 'axios',  
+    },
+  },
+  build: {
+    rollupOptions: {
+      // Excluir módulos específicos del bundle si es necesario
+      external: [],  // Deja vacío o especifica módulos si los tienes
+    },
+  },
 })
