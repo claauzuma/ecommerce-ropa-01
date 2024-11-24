07429b75a041a2f166e5fba6b71bb77ab693abe6
claauzuma <c.zumarraga94@hotmail.com>
2024-11-24 17:54:52 -0300

    Agregando tabla de comentarioss

--- proyecto01/src/components/ResumenFinanciero.jsx
+++ proyecto01/src/components/ResumenFinanciero.jsx
@@ -1,59 +1,114 @@
-import React, { useState } from 'react';
-import { Container, Button, Table, Form } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Button, Table, Form, Modal, Row, Col } from 'react-bootstrap';
 import NavBar from '../pages/Admin/NavBar.jsx';
+import ApiUrls from './ApiUrls.jsx';
+import Select from 'react-select';
 
 const ResumenFinanciero = () => {
-  const [vistaMes, setVistaMes] = useState('Ultimos 5 meses'); // Vista predeterminada
+  const [vistaMes, setVistaMes] = useState('Ultimos 5 meses');
+  const [showVentaModal, setShowVentaModal] = useState(false);
+  const [showGastoModal, setShowGastoModal] = useState(false);
+  const [productos, setProductos] = useState([]);  // Lista original de productos desde la API
+  const [productosSeleccionados, setProductosSeleccionados] = useState([{ nombre: '', talle: '', color: '', cantidad: '' }]); // Inicializar con un producto vacío
+  const [productoElegido, setProductoElegido] = useState();
+  const [talleElegido, setTalleElegido] = useState();
+  const [colorElegido, setColorElegido] = useState();
+ 
 
-  // Datos por mes
+  const [gastos, setGastos] = useState([{ detalle: '', cantidad: '', valor: '' }]);
+  const [mesSeleccionado, setMesSeleccionado] = useState('');
+  const [showDetalleModal, setShowDetalleModal] = useState(false); // Estado para manejar el modal de detalle
+
+  // Datos de ejemplo para los meses
   const datosPorMes = {
     Enero: { ventas: 3000, gastos: 1500 },
     Febrero: { ventas: 2500, gastos: 1200 },
     Marzo: { ventas: 3200, gastos: 1700 },
-    Abril: { ventas: 2800, gastos: 1400 },
-    Mayo: { ventas: 3500, gastos: 1600 },
-    Junio: { ventas: 2900, gastos: 1300 },
+    Abril: { ventas: 2900, gastos: 1400 },
+    Mayo: { ventas: 3100, gastos: 1600 },
+    Junio: { ventas: 2800, gastos: 1300 },
     Julio: { ventas: 3000, gastos: 1500 },
-    Agosto: { ventas: 3400, gastos: 1800 },
-    Septiembre: { ventas: 3100, gastos: 1600 },
-    Octubre: { ventas: 3300, gastos: 1700 },
-    Noviembre: { ventas: 2900, gastos: 1400 },
-    Diciembre: { ventas: 3500, gastos: 1800 },
+    Agosto: { ventas: 3200, gastos: 1800 },
+    Septiembre: { ventas: 3400, gastos: 2000 },
+    Octubre: { ventas: 3500, gastos: 2100 },
+    Noviembre: { ventas: 3600, gastos: 2200 },
   };
 
-  // Obtener los últimos 5 meses
   const obtenerUltimos5Meses = () => {
-    const meses = Object.keys(datosPorMes);
-    return meses.slice(-5); // Últimos 5 meses
+    const mesesOrdenados = Object.keys(datosPorMes);
+    return mesesOrdenados.slice(-5);
   };
 
-  const mesesAMostrar = vistaMes === 'Todos' ? Object.keys(datosPorMes) : obtenerUltimos5Meses();
+  const obtenerMesesHastaFechaActual = () => {
+    const mesesOrdenados = Object.keys(datosPorMes);
+    const fechaActual = new Date();
+    const mesActual = fechaActual.getMonth(); // Índice del mes actual (0 = Enero, 1 = Febrero, etc.)
+    return mesesOrdenados.slice(0, mesActual + 1);  // Incluir el mes actual
+  };
 
-  // Función para calcular el porcentaje de cambio
-  const calcularVariacionPorcentual = (mesKey, balanceActual) => {
-    const mesIndex = Object.keys(datosPorMes).indexOf(mesKey);
-    if (mesIndex === 0) return null; // No hay mes anterior para el primer mes
+  const mesesAMostrar = vistaMes === 'Todos' ? obtenerMesesHastaFechaActual() : obtenerUltimos5Meses();
 
-    const mesAnterior = Object.keys(datosPorMes)[mesIndex - 1];
-    const balanceAnterior = datosPorMes[mesAnterior].ventas - datosPorMes[mesAnterior].gastos;
-    const variacion = ((balanceActual - balanceAnterior) / balanceAnterior) * 100;
+  // Obtener productos desde la API
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const response = await fetch(`${ApiUrls.productos}`);
+        const data = await response.json();
+        setProductos(data);  // Cargar productos originales
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
 
-    return variacion.toFixed(2); // Redondear a 2 decimales
+    fetchProductos();
+  }, []);
+
+  const handleProductoSeleccionado = (selectedProductName) => {
+    const productoSeleccionado = productos.find(
+      (prod) => prod.nombre === selectedProductName
+    );
+    console.log("El producto seleccionado es ", productoSeleccionado)
+    
+    // Setear el producto en el estado productoElegido
+    setProductoElegido(productoSeleccionado);
+  };
+  
+
+  // Funciones para manejar productos seleccionados
+  const handleAgregarProducto = () => {
+    setProductosSeleccionados([...productosSeleccionados, { nombre: '', talle: '', color: '', cantidad: '' }]);
+  };
+
+  const handleEliminarProducto = (index) => {
+    const nuevosProductos = productosSeleccionados.filter((_, i) => i !== index);
+    setProductosSeleccionados(nuevosProductos);
+  };
+
+  const handleProductoChange = (index, field, value) => {
+    const nuevosProductos = [...productosSeleccionados]; // Creamos una copia del array
+    nuevosProductos[index][field] = value; // Actualizamos el campo específico (talle, color, etc.)
+    setProductosSeleccionados(nuevosProductos); // Actualizamos el estado
+  };
+  
+
+  const handleGuardarVenta = () => {
+    console.log(`Productos guardados para ${mesSeleccionado}:`, productosSeleccionados);
+    setShowVentaModal(false);
+    setProductosSeleccionados([]);  // Limpiar después de guardar
+  };
+
+  const handleVerDetalle = (mes) => {
+    setMesSeleccionado(mes);
+    setShowDetalleModal(true); // Mostrar modal de detalle
   };
 
   return (
     <>
       <NavBar />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Container className="mt-4">
+      <Container className="mt-5 pt-5 pb-5">
         <h2 className="text-center mb-4 text-dark">Resumen Financiero</h2>
 
-        {/* Selector de vista */}
         <Form.Group className="mb-4">
           <Form.Label className="text-dark">Mostrar</Form.Label>
           <Form.Control
@@ -67,65 +122,148 @@ const ResumenFinanciero = () => {
           </Form.Control>
         </Form.Group>
 
-        {/* Tabla de resumen financiero con scroll horizontal */}
-        <div className="table-responsive" style={{ overflowX: 'auto' }}>
-          <Table striped bordered hover className="text-dark">
-            <thead className="bg-dark text-white">
-              <tr>
-                <th>Mes</th>
-                <th>Ventas</th>
-                <th>Gastos</th>
-                <th>Balance</th>
-                <th>Variación</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {mesesAMostrar.map((mesKey) => {
-                const { ventas, gastos } = datosPorMes[mesKey];
-                const balance = ventas - gastos; // Calcular el balance
-                const variacion = calcularVariacionPorcentual(mesKey, balance); // Calcular la variación porcentual
-                const porcentaje = variacion !== null ? (variacion >= 0 ? `+${variacion}%` : `${variacion}%`) : '';
+        <Table striped bordered hover className="text-dark">
+          <thead className="bg-dark text-white">
+            <tr>
+              <th>Mes</th>
+              <th>Ventas</th>
+              <th>Gastos</th>
+              <th>Balance</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mesesAMostrar.map((mesKey) => {
+              const { ventas, gastos } = datosPorMes[mesKey];
+              const balance = ventas - gastos;
 
-                return (
-                  <tr key={mesKey}>
-                    <td>{mesKey}</td>
-                    <td>${ventas}</td>
-                    <td>${gastos}</td>
-                    <td>${balance}</td>
-                    <td>{porcentaje}</td> {/* Mostrar la variación */}
-                    <td className="d-flex">
-                      <Button
-                        variant="outline-warning"
-                        className="me-2"
-                        size="sm"
-                        onClick={() => alert(`Agregar venta en ${mesKey}`)}
-                      >
-                        + Venta
-                      </Button>
-                      <Button
-                        variant="outline-danger"
-                        className="me-2"
-                        size="sm"
-                        onClick={() => alert(`Agregar gasto en ${mesKey}`)}
-                      >
-                        + Gasto
-                      </Button>
-                      <Button
-                        variant="outline-info"
-                        size="sm"
-                        onClick={() => alert(`Detalle de ${mesKey}`)}
-                      >
-                        Detalle
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </div>
+              return (
+                <tr key={mesKey}>
+                  <td>{mesKey}</td>
+                  <td>${ventas}</td>
+                  <td>${gastos}</td>
+                  <td>${balance}</td>
+                  <td className="d-flex">
+                    <Button
+                      variant="outline-warning"
+                      className="me-2"
+                      size="sm"
+                      onClick={() => { setShowVentaModal(true); setMesSeleccionado(mesKey); }}
+                    >
+                      + Venta
+                    </Button>
+                    <Button
+                      variant="outline-danger"
+                      className="me-2"
+                      size="sm"
+                      onClick={() => { setShowGastoModal(true); setMesSeleccionado(mesKey); }}
+                    >
+                      + Gasto
+                    </Button>
+                    <Button
+                      variant="outline-info"
+                      className="me-2"
+                      size="sm"
+                      onClick={() => handleVerDetalle(mesKey)} // Mostrar modal de detalle
+                    >
+                      Detalle
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </Container>
+
+      {/* Modal para agregar productos */}
+      <Modal show={showVentaModal} onHide={() => setShowVentaModal(false)} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Agregar Venta - {mesSeleccionado}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {productosSeleccionados.map((producto, index) => (
+            <Row key={index} className="mb-3">
+ <Col>
+  <Select
+    options={productos.map((prod) => ({ value: prod.nombre, label: prod.nombre }))}
+    value={{ value: productoElegido?.nombre || '', label: productoElegido?.nombre || '' }} // Si no hay un producto elegido, mostramos vacío
+    onChange={(selectedOption) => handleProductoSeleccionado(selectedOption.value)} // Llamamos a la función para setear el producto
+    placeholder="Seleccionar producto"
+  />
+</Col>
+<Col>
+  <Select
+    options={productoElegido?.talles?.map((item) => ({ value: item.talle, label: item.talle })) || []}
+    value={{ value: producto.talle, label: producto.talle }} // Asegúrate de que el valor esté correctamente asignado
+    onChange={(selectedOption) => handleProductoChange(index, 'talle', selectedOption.value)} // Usamos selectedOption.value
+    placeholder="Seleccionar talle"
+  />
+</Col>
+
+
+
+              <Col>
+                <Form.Control
+                  placeholder="Color"
+                  value={producto.color}
+                  onChange={(e) => handleProductoChange(index, 'color', e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  placeholder="Cantidad"
+                  type="number"
+                  value={producto.cantidad}
+                  onChange={(e) => handleProductoChange(index, 'cantidad', e.target.value)}
+                />
+              </Col>
+              <Col xs="auto">
+                <Button variant="danger" onClick={() => handleEliminarProducto(index)}>
+                  Eliminar
+                </Button>
+              </Col>
+            </Row>
+          ))}
+          <Button variant="success" onClick={handleAgregarProducto}>
+            + Agregar otro producto
+          </Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowVentaModal(false)}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleGuardarVenta}>
+            Guardar Venta
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Modal para ver detalles */}
+
+<Modal show={showDetalleModal} onHide={() => setShowDetalleModal(false)} size="lg">
+  <Modal.Header closeButton>
+    <Modal.Title>Detalle de {mesSeleccionado}</Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    {datosPorMes[mesSeleccionado] ? ( // Verificar si el mes existe
+      <>
+        <h5>Detalles de ventas y gastos para {mesSeleccionado}</h5>
+        <p>Ventas: ${datosPorMes[mesSeleccionado].ventas}</p>
+        <p>Gastos: ${datosPorMes[mesSeleccionado].gastos}</p>
+        <p>Balance: ${datosPorMes[mesSeleccionado].ventas - datosPorMes[mesSeleccionado].gastos}</p>
+      </>
+    ) : (
+      <p>No se encontraron datos para el mes seleccionado.</p> // Mensaje en caso de que no haya datos
+    )}
+  </Modal.Body>
+  <Modal.Footer>
+    <Button variant="secondary" onClick={() => setShowDetalleModal(false)}>
+      Cerrar
+    </Button>
+  </Modal.Footer>
+</Modal>
+
     </>
   );
 };
